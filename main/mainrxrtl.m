469fb4e188da914f995c535bb0f370d6c3ef5f5f
andewx <brian.michael.anderson@gmail.com>
2025-05-06 17:49:44 -0700

    Working RTL-SDR

--- main/mainrxrtl.m
+++ main/mainrxrtl.m
@@ -3,21 +3,23 @@
 % This generates a test signal and powers it through SDR Hardware Frontend
 % and Receives and Demodulates the Test GPS GNSS C/A Signal on the Q
 % Branch.
- thisDir = fileparts(mfilename('fullpath'));
- parentDir = fullfile(thisDir, '..');
-
-
+thisDir = fileparts(mfilename('fullpath'));
+parentDir = fullfile(thisDir, '..');
 addpath(parentDir);
+
+
+SAMPLES_PER_CHIP = 2;
+fs = 1.023e6*SAMPLES_PER_CHIP; % Sample rate
+DATA_RATE = 20; % 20ms 
+NUM_DATA_BITS = 5;
+SAMPLE_TIME = (DATA_RATE*NUM_DATA_BITS)*1e-3;
+SAMPLES = floor(SAMPLE_TIME*fs);
+
 % Create an instance of the FLL class before ach
 DSP = GPSSignalProcessor(3,SAMPLES_PER_CHIP,1.023e6*SAMPLES_PER_CHIP, 0);
 
 % Test updating the output of the DLL
-SAMPLES_PER_CHIP = 2;
-fs = 1.023e6*SAMPLES_PER_CHIP; % Sample rate
-DATA_RATE = 20; % 20ms 
-NUM_DATA_BITS = 10;
-SAMPLE_TIME = (DATA_RATE*NUM_DATA_BITS)*1e-3;
-SAMPLES = floor(SAMPLE_TIME*fs);
+
 
 % Send Through Pluto Hardware at 920MHZ
 fc = 920.1e6;
@@ -31,7 +33,7 @@ rx = comm.SDRRTLReceiver('CenterFrequency',fc,...
       'FrequencyCorrection',0);
 
 % Setup a scope to visualize the values
-scope = timescope('SampleRate', obj.sampleRate/1023, 'TimeSpan', length(samples)*(1/obj.sampleRate), 'TimeSpanSource', "property");
+scope = timescope('SampleRate', fs/1023, 'TimeSpan', SAMPLES*(1/fs), 'TimeSpanSource', "property");
 
 % Acquire the signal
 disp('Acquiring Signal...');
